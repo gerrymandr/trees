eecbf5744457f839960f8687e51f16a1ecf48da9
gabeschoenbach <gabe.schoenbach@gmail.com>
2021-01-18 10:52:25 -0500

    add alternate edge orderings

--- ZDD/jl_zdd/edge_ordering.jl
+++ ZDD/jl_zdd/edge_ordering.jl
@@ -32,7 +32,7 @@ function nw_diag(x,y,x_max,y_max)
     return edge_list
 end
 
-function optimal_grid_edge_order(g::SimpleGraph, r::Int, c::Int)
+function optimal_grid_edge_order_diags(g::SimpleGraph, r::Int, c::Int)
     x_max = c-1
     y_max = r-1
     python_edge_list = Tuple{Tuple{Int, Int}, Tuple{Int,Int}}[]
@@ -60,6 +60,121 @@ function optimal_grid_edge_order(g::SimpleGraph, r::Int, c::Int)
     end
     return julia_edge_list
 end
+
+function optimal_grid_edge_order_rows(g::SimpleGraph, r::Int, c::Int)
+    x_max = c-1
+    y_max = r-1
+    python_edge_list = Tuple{Tuple{Int,Int}, Tuple{Int,Int}}[]
+    if x_max >= y_max
+        for x ∈ 0:x_max-1
+            for y ∈ 0:y_max-1
+                push!(python_edge_list, ((x,y),(x,y+1)))
+            end
+            for y ∈ 0:y_max
+                push!(python_edge_list, ((x,y),(x+1,y)))
+            end
+        end
+        for y ∈ 0:y_max-1
+            push!(python_edge_list, ((x_max,y),(x_max,y+1)))
+        end
+    else
+        for y ∈ 0:y_max-1
+            for x ∈ 0:x_max-1
+                push!(python_edge_list, ((x,y),(x+1,y)))
+            end
+            for x ∈ 0:x_max
+                push!(python_edge_list, ((x,y),(x,y+1)))
+            end
+        end
+        for x ∈ 0:x_max-1
+            push!(python_edge_list, ((x,y_max),(x+1,y_max)))
+        end
+    end
+    julia_edge_list = [convert_python_edges_to_julia(edge, r) for edge in python_edge_list]
+    @assert length(julia_edge_list) == length(edges(g))
+    for e ∈ julia_edge_list
+        @assert e ∈ edges(g)
+    end
+    return julia_edge_list
+end
+
+function optimal_grid_edge_order_boxes_1(g::SimpleGraph, r::Int, c::Int)
+    x_max = c-1
+    y_max = r-1
+    python_edge_list = Tuple{Tuple{Int,Int}, Tuple{Int,Int}}[]
+    if x_max >= y_max
+        for y ∈ 0:y_max-1
+            push!(python_edge_list, ((0,y),(0,y+1)))
+        end
+        for x ∈ 0:x_max-1
+            push!(python_edge_list, ((x,0),(x+1,0)))
+            for y ∈ 0:y_max-1
+                push!(python_edge_list, ((x,y+1),(x+1,y+1)))
+                push!(python_edge_list, ((x+1,y),(x+1,y+1)))
+            end
+        end
+    else
+        for x ∈ 0:x_max-1
+            push!(python_edge_list, ((x,0),(x+1,0)))
+        end
+        for y ∈ 0:y_max-1
+            push!(python_edge_list, ((0,y),(0,y+1)))
+            for x ∈ 0:x_max-1
+                push!(python_edge_list, ((x+1,y),(x+1,y+1)))
+                push!(python_edge_list, ((x,y+1),(x+1,y+1)))
+            end
+        end
+    end
+    julia_edge_list = [convert_python_edges_to_julia(edge, r) for edge in python_edge_list]
+    @assert length(julia_edge_list) == length(edges(g))
+    for e ∈ julia_edge_list
+        @assert e ∈ edges(g)
+    end
+    return julia_edge_list
+end
+
+function optimal_grid_edge_order_boxes_2(g::SimpleGraph, r::Int, c::Int)
+    x_max = c-1
+    y_max = r-1
+    python_edge_list = Tuple{Tuple{Int,Int}, Tuple{Int,Int}}[]
+    if x_max >= y_max
+        push!(python_edge_list, ((0,0),(1,0)))
+        for y ∈ 0:y_max-1
+            push!(python_edge_list, ((0,y),(0,y+1)))
+            push!(python_edge_list, ((0,y+1),(1,y+1)))
+            push!(python_edge_list, ((1,y),(1,y+1)))
+        end
+        for x ∈ 1:x_max-1
+            push!(python_edge_list, ((x,0),(x+1,0)))
+            for y ∈ 0:y_max-1
+                push!(python_edge_list, ((x,y+1),(x+1,y+1)))
+                push!(python_edge_list, ((x+1,y),(x+1,y+1)))
+            end
+        end
+    else
+        push!(python_edge_list, ((0,0),(0,1)))
+        for x ∈ 0:x_max-1
+            push!(python_edge_list, ((x,0),(x+1,0)))
+            push!(python_edge_list, ((x+1,0),(x+1,1)))
+            push!(python_edge_list, ((x,1),(x+1,1)))
+        end
+        for y ∈ 1:y_max-1
+            push!(python_edge_list, ((0,y),(0,y+1)))
+            for x ∈ 0:x_max-1
+                push!(python_edge_list, ((x+1,y),(x+1,y+1)))
+                push!(python_edge_list, ((x,y+1),(x+1,y+1)))
+            end
+        end
+    end
+    julia_edge_list = [convert_python_edges_to_julia(edge, r) for edge in python_edge_list]
+    @assert length(julia_edge_list) == length(edges(g))
+    for e ∈ julia_edge_list
+        @assert e ∈ edges(g)
+    end
+    return julia_edge_list
+end
+
+
 
 function optimal_queen_grid_edge_order(g::SimpleGraph, r::Int, c::Int)
     x_max = c-1
